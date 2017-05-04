a817c8cd52f4522b289ea471e63be3809cfed9e4
lucianoMarchezan <augustus.marchezan@gmail.com>
2017-05-04 20:35:39 -0300

    Adding the save functions and make diagram auto load/save
    Save as .Json or .Png is working. Still need to do upload.

--- Protótipo Atlas/sourceAtlasPrototype/js/BackwardParserScript.js
+++ Protótipo Atlas/sourceAtlasPrototype/js/BackwardParserScript.js
@@ -1,38 +1,40 @@
-function parseToModel(string){
-var m1 = new Model("m1");
-var lines = string.split('\n');
-for(var i = 0;i < lines.length;i++){
-	
-	if(lines[i].indexOf("\"key\"")!=-1){
-		if(lines[i].indexOf("category\":\"LinkLabel")==-1){
-			var indexA = lines[i].lastIndexOf("\"key\":\"")+7;
-			var indexB = lines[i].indexOf("\", \"");
-			var indexC = lines[i].lastIndexOf("\"category\":\"")+12;
-			var indexD = lines[i].indexOf("\"},");
-			var name = lines[i].slice(indexA,indexB);
-			var type = lines[i].slice(indexC,indexD);
-			m1.addFeature(new Feature(name, type));
-		}
+
+
+	function parseToModel(string){
+	var m1 = new Model("m1");
+	var lines = string.split('\n');
+	for(var i = 0;i < lines.length;i++){
 		
-	}
-	if(lines[i].indexOf("\"from\":")!=-1){
-		if(lines[i].indexOf("category\":\"linkToLink")==-1){
-			var indexA = lines[i].lastIndexOf("\"from\":\"")+8;
-			var indexB = lines[i].indexOf("\", \"to\"");
-			var f1Name = lines[i].slice(indexA,indexB);
-						
-			var indexC = lines[i].lastIndexOf("\"to\":\"")+6;
-			var indexD = lines[i].indexOf("\", \"labelKeys\"");
-			var f2Name = lines[i].slice(indexC,indexD);
+		if(lines[i].indexOf("\"key\"")!=-1){
+			if(lines[i].indexOf("category\":\"LinkLabel")==-1){
+				var indexA = lines[i].lastIndexOf("\"key\":\"")+7;
+				var indexB = lines[i].indexOf("\", \"");
+				var indexC = lines[i].lastIndexOf("\"category\":\"")+12;
+				var indexD = lines[i].indexOf("\"},");
+				var name = lines[i].slice(indexA,indexB);
+				var type = lines[i].slice(indexC,indexD);
+				m1.addFeature(new Feature(name, type));
+			}
 			
-			var f1 = m1.findFeature(f1Name);	
-			var f2 = m1.findFeature(f2Name);
-			var a1 = new Association(f1,f2);
-			m1.addAssociation(a1);
+		}
+		if(lines[i].indexOf("\"from\":")!=-1){
+			if(lines[i].indexOf("category\":\"linkToLink")==-1){
+				var indexA = lines[i].lastIndexOf("\"from\":\"")+8;
+				var indexB = lines[i].indexOf("\", \"to\"");
+				var f1Name = lines[i].slice(indexA,indexB);
+							
+				var indexC = lines[i].lastIndexOf("\"to\":\"")+6;
+				var indexD = lines[i].indexOf("\", \"labelKeys\"");
+				var f2Name = lines[i].slice(indexC,indexD);
+				
+				var f1 = m1.findFeature(f1Name);	
+				var f2 = m1.findFeature(f2Name);
+				var a1 = new Association(f1,f2);
+				m1.addAssociation(a1);
+			}
 		}
 	}
-}
 
-return m1;
+	return m1;
 
-}
+	}
--- Protótipo Atlas/sourceAtlasPrototype/js/DownloadScript.js
+++ Protótipo Atlas/sourceAtlasPrototype/js/DownloadScript.js
@@ -2,6 +2,42 @@ function downloadContent(name, content) {
   var atag = document.createElement("a");
   var file = new Blob([content], {type: 'text/plain'});
   atag.href = URL.createObjectURL(file);
-  atag.download = name;
+  atag.download = name +'.json';
   atag.click();
 }
+
+function downloadImg(name, content) {
+	
+	var binary = fixBinary(atob(content));
+	var blob = new Blob([binary], {type: 'image/png'});
+    var url = window.URL.createObjectURL(blob);
+    var filename = name+".png";
+
+    var a = document.createElement("a");
+    a.style = "display: none";
+    a.href = url;
+    a.download = filename;
+
+    // IE 11
+    if (window.navigator.msSaveBlob !== undefined) {
+      window.navigator.msSaveBlob(content, filename);
+      return;
+    }
+
+    document.body.appendChild(a);
+    requestAnimationFrame(function() {
+      a.click();
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    });
+  }
+  
+  function fixBinary (bin) {
+    var length = bin.length;
+    var buf = new ArrayBuffer(length);
+    var arr = new Uint8Array(buf);
+    for (var i = 0; i < length; i++) {
+      arr[i] = bin.charCodeAt(i);
+    }
+    return buf;
+  }

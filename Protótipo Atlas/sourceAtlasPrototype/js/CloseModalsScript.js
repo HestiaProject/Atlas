8efef07273f29e29f88f5cca8984659c52db9d5b
Lucas Martins <lgatts@gmail.com>
2017-05-10 17:27:08 -0300

    Changed modals style, added close button in all modals, make part of create association.

---
+++ Protótipo Atlas/sourceAtlasPrototype/js/CloseModalsScript.js
@@ -0,0 +1,11 @@
+function closeFileModalButtonClick(){
+    document.getElementById("loadFileModal").style.display = "none";
+}
+
+function closeAssociationModalButtonClick(){
+    document.getElementById("associationModal").style.display = "none";
+}
+
+function closeFeatureModalButtonClick(){
+    document.getElementById("featureModal").style.display = "none";
+}
--- Protótipo Atlas/sourceAtlasPrototype/js/CreateModals.js
+++ Protótipo Atlas/sourceAtlasPrototype/js/CreateModals.js
@@ -1,4 +1,4 @@
-
+// Create Feature Modal
 function createFeatureModalButtonClick() {
     var createFeatureModal = document.getElementById("featureModal");
     var comboBox = document.getElementById("featureTypeComboBox");
@@ -16,10 +16,34 @@ function createFeatureModalButtonClick() {
         }
     }
 
-    createFeatureModal.style.display = "block";
+    createFeatureModal.style.display = "block";   
     document.getElementById("nameFeatureTextField").focus();
+    
 }
 
+// Create Association Modal
+
+function createAssociationModalButtonClick() {
+    var createAssociationModal = document.getElementById("associationModal");
+    var feature1ComboBox = document.getElementById("feature1ComboBox");
+    var feature2ComboBox = document.getElementById("feature2ComboBox");
+
+    var feature1Name;
+    var feature2Name;
+
+    m1.getFeatures().forEach(function (feature) {
+        feature1Name = feature.getName();
+        option = document.createElement("option");
+        option.text = feature1Name;
+        feature1ComboBox.add(option);
+    });
+
+    createAssociationModal.style.display = "block";
+
+}
+
+
+// Create Load Modal
 function createLoadModalButtonClick() {
     var createLoadModal = document.getElementById("loadFileModal");
     createLoadModal.style.display = "block";

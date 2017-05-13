4ccb5e5a215f4f2c73b63cdf4c78f831bbaa2a7d
lucianoMarchezan <augustus.marchezan@gmail.com>
2017-05-13 20:20:01 -0300

    Adding try-catch when upload .json

--- Protótipo Atlas/sourceAtlasPrototype/js/InitialScript.js
+++ Protótipo Atlas/sourceAtlasPrototype/js/InitialScript.js
@@ -2,16 +2,16 @@ function init(m1){
 
 			var f1 = new Feature("Root", "mandatory");
 			var f2 = new Feature("Child", "mandatory");
-			var f3 = new Feature("Chfild", "mandatory");
+			//var f3 = new Feature("Chfild", "mandatory");
 			
 			var a1 = new Association(f1, f2);
-			var a2 = new Association(f1, f3);
+			//var a2 = new Association(f1, f3);
 			m1.addFeature(f1);
-			m1.addFeature(f2);
-			m1.addFeature(f3);
+			//m1.addFeature(f2);
+			//m1.addFeature(f3);
 			
 			m1.addAssociation(a1);
-			m1.addAssociation(a2);
+			//m1.addAssociation(a2);
 			
 			document.getElementById('mySavedModel').value = parseModelToString(m1);
 			

896a7cfad763887ade8732d5c355f7814352e4cf
Lucas Martins <lgatts@gmail.com>
2017-05-17 08:53:20 -0300

    Better rize components

--- Protótipo Atlas/sourceAtlasPrototype/js/AdjustCanvas.js
+++ Protótipo Atlas/sourceAtlasPrototype/js/AdjustCanvas.js
@@ -1,16 +1,31 @@
-var canvas = document.getElementById("myDiagramDiv");
-
-function adjustCanvas(){
+function adjustSizes() {
+    var canvas = document.getElementById("myDiagramDiv");
+    var topBarIcons = document.getElementById("topBarIconGroup");
+    var topBarIconsSmall = document.getElementById("topBarIconGroupSmall");
+    var modelName = document.getElementById("modelName");
     var height = window.innerHeight;
     var width = window.innerWidth;
 
-    width -= 90;
-    height -= 90;   
+    canvasWidth = width - 90;
+    canvasHeight = height - 95;
 
-    canvas.style.width = width.toString()+"px";
-    canvas.style.height = height.toString()+"px";
+    canvas.style.width = canvasWidth.toString() + "px";
+    canvas.style.height = canvasHeight.toString() + "px";
+
+
+    modelName.style.marginLeft = (width / 2) - (modelName.clientWidth / 2) + "px";
+
+    if (width <= 600) {
+        // topBarIcons.style.display = "block";
+        topBarIcons.style.display = "none";
+        topBarIconsSmall.style.display = "block";
+    } else {
+        topBarIcons.style.display = "block";
+        topBarIconsSmall.style.display = "none";
+    }
+
 }
 
-window.onresize = function(){
-    adjustCanvas();
+window.onresize = function() {
+    adjustSizes();
 }
